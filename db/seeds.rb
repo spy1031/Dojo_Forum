b371656d7f0962453464e5a955aa7d353e19c7b7
spy <sp831031@gmail.com>
2018-04-20 11:39:27 +0800

    add default user and category in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#================== Create default user ==============#
+User.create(name: "admin", email: "admin@example.com", password: "12345678", role: "admin")
+
+#================== Create default category =============#
+category_list = %w(戰士 薩滿 盜賊 聖騎士 獵人 德魯伊 術師 法師 牧師)
+
+category_list.each do |category|
+  Category.create(content: category)
+end
