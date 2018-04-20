bcab1669b333c6e74104a7686566a26843372611
spy <sp831031@gmail.com>
2018-04-20 11:51:18 +0800

    rename column type to name in categoies table

---
+++ db/migrate/20180420034750_rename_type_in_category.rb
@@ -0,0 +1,5 @@
+class RenameTypeInCategory < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :categories, :type, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418125339) do
+ActiveRecord::Schema.define(version: 20180420034750) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180418125339) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
