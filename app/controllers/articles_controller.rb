28167a71bcbbcf4ef224eccf8312f5cf948f4cc5
spy <sp831031@gmail.com>
2018-04-21 15:58:22 +0800

    add acticle#new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
-
+  before_action :authenticate_user!
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
   end
 end
