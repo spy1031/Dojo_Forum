4103c9df32f7ee82e9192afe644179384c4dbdc6
spy <sp831031@gmail.com>
2018-04-22 15:15:40 +0800

    user can create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,28 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    @article.last_reply_time = Time.now.localtime
+    if params[:draft]
+      @article.status = 0
+    else
+      @article.status = 1
+    end
+
+    @article.save!
+    flash[:notice] = @article.status ? "成功發布文章" : "成功儲存草稿"
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :content,
+      :authority,
+      :category_id)
+  end
 end
