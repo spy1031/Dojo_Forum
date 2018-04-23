5d90c2d20623a4df432e7fb21bcb272f57b32779
spy <sp831031@gmail.com>
2018-04-23 17:39:28 +0800

    add uses#edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user
 
+  def edit
+    if @user != current_user 
+      flash[:alert] = "權限錯誤"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
   def articles
     @articles = @user.articles.where("status = ? ", true)
   end
