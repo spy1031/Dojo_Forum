c1e790a891c2bccdcba2a2749716fe4e2e13dfae
spy <sp831031@gmail.com>
2018-04-23 19:44:19 +0800

    add image column to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423101818) do
+ActiveRecord::Schema.define(version: 20180423114317) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180423101818) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["last_reply_time"], name: "index_articles_on_last_reply_time"
     t.index ["user_id"], name: "index_articles_on_user_id"
