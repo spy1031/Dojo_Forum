ba90b2045841b060cd03fccf7c9153adbb8f7e4b
spy <sp831031@gmail.com>
2018-04-23 18:19:42 +0800

    add avatar column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423031918) do
+ActiveRecord::Schema.define(version: 20180423101818) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180423031918) do
     t.string "name"
     t.string "introduction"
     t.string "gender"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
