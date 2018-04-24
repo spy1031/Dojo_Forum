efd81e34d91e3f551f16feec69fbf601a1a61779
spy <sp831031@gmail.com>
2018-04-24 11:32:06 +0800

    enter except index page must login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_article, only: [:show, :destroy]
   def index
     @articles = Article.where("status = ?", true).page(params[:page]).per(20)
