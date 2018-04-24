d3daf5b2632de09b32907f432fac4eaadd77d404
spy <sp831031@gmail.com>
2018-04-24 15:23:33 +0800

    setup user friendships reference relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
   has_many :replies, dependent: :destroy
-  has_many :friendships
+  has_many :friendships, ->{where status: 3}, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, ->{where status: 3}, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :invers_friends, through: :inverse_friendships, source: :user
+
+  has_many :friend_requests, ->{where status: 2}, class_name: "Friendship", dependent: :destroy
+  has_many :requests, through: :friend_requests, source: :user
+  has_many :invites, through: :friend_requests, source: :friend
 
   def admin?
     self.role == "admin"
