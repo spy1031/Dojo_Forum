d101cacf8da21d7bfaf8fb87bf1ed79963933fe6
spy <sp831031@gmail.com>
2018-04-24 12:09:27 +0800

    add categories#show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,14 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+
+  def show
+    @articles = @category.articles.page(params[:page]).per(20)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
