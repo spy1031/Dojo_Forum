96ad1d5fc94c0aac8d601b8568e81ad443999a19
spy <sp831031@gmail.com>
2018-04-25 12:36:26 +0800

    setup collections route

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :replies, only: [:create, :destroy, :update]
   resources :categories, only: [:show]
   resources :friendships
+  resources :collections, only: [:create, :destroy]
 end
