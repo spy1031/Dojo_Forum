0d82529c5fe0d795f424ae8237251532ab0cd7ac
spy <sp831031@gmail.com>
2018-04-26 19:51:17 +0800

    admin user can add new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,4 +3,16 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
     @category = Category.new
   end
+
+  def create
+    @category = Category.create(category_params)
+    flash[:notice] = "成功新增文章分類"
+    redirect_to admin_root_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
