d9bc5b852eb88cbcc9b062f27df27d231d20a09b
spy <sp831031@gmail.com>
2018-04-26 17:25:16 +0800

    setup admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    root "categories#index"
+    resources :categories, only: [:index, :create, :update, :destroy]
+    resources :users,only: [:index, :update]
+  end
+
+
   root "articles#index"
   resources :articles do
     collection do
