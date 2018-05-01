a6ce43be9bf36037157bbef62a58c4d63539042d
spy <sp831031@gmail.com>
2018-05-01 17:55:38 +0800

    setup api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do 
+      resources :articles, only: [:index, :show, :create, :update, :destroy]
+    end
+
+  end
+
   namespace :admin do
     root "categories#index"
     resources :categories, only: [:index, :create, :update, :destroy]
