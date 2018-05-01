aa82bf74ec4dcef6cba2b4a40104cb93f1316ddc
spy <sp831031@gmail.com>
2018-05-01 18:03:02 +0800

    add index action for article api

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::ArticlesController < ApiController
+  def index
+    if current_user == nil
+      @articles = Article.where("status = ? AND authority = ?", true, 1)
+    else 
+      @articles = Article.where("status = ?", true).check_authority(current_user)
+    end
+    render json: @articles
+  end
 end
