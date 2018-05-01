27bbadb2f8e086651efd2a8a1bbbcb0045097e64
spy <sp831031@gmail.com>
2018-05-01 17:25:21 +0800

    add validates friendship's user_id and friend_id to make sure no duplicate

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
+  validates :friend_id, uniqueness: { scope: :user_id }
+  validates :user_id, uniqueness: { scope: :friend_id }
   belongs_to :user
   belongs_to :friend, class_name: "User"
 end
