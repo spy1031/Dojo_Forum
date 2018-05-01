54790c14cf81dad5f71af3a027039ff8627acd50
spy <sp831031@gmail.com>
2018-05-01 18:54:44 +0800

    add generata_authentication_token method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,4 +42,8 @@ class User < ApplicationRecord
   def collector?(article)
     article.collectors.include?(self)
   end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
 end
