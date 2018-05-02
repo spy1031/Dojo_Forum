a84e06a51616cc8b90074ee1247386a61b27b1b0
spy <sp831031@gmail.com>
2018-05-02 16:23:03 +0800

    create article_category model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502074538) do
+ActiveRecord::Schema.define(version: 20180502081433) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id", "category_id"], name: "index_article_categories_on_article_id_and_category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
