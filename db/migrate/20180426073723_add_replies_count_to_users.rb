ed36b780bf411e7048b9d99306cd799ca84d45e9
spy <sp831031@gmail.com>
2018-05-02 15:16:21 +0800

    fix add_index problem

--- db/migrate/20180426073723_add_replies_count_to_users.rb
+++ db/migrate/20180426073723_add_replies_count_to_users.rb
@@ -1,6 +1,6 @@
 class AddRepliesCountToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :replies_count, :integer, default: 0
+    add_index :users, :replies_count
   end
-  add_index :users, :replies_count
 end
