0debbfc5244be5b14639b772c03f9b1234bceea0
spy <sp831031@gmail.com>
2018-05-02 10:38:47 +0800

    setup factory-bot and add factories

---
+++ spec/factories.rb
@@ -0,0 +1,28 @@
+Factory.defin do 
+  factory :user do
+    sequence(:name) { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { "12345678" }
+    introduction { FFaker::Lorem.paragraph }
+    role { "normal" }
+  end
+
+  factory :category do 
+    sequence(:name) { |n| "category#{n}" }
+  end
+
+  factory :article do 
+    sequence(:title) { |n| "article#{n}" }
+    content { FFaker::Lorem.paragraph }
+    authority
+    status
+    category
+  end
+
+  factory :reply do 
+    content { FFaker::Lorem.paragraph}
+    user
+    article
+  end
+
+end 
