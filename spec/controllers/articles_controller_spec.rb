9bcb2c42024e3365271c3e5e6348ce6c2d0af647
spy <sp831031@gmail.com>
2018-05-03 16:24:03 +0800

    add create_article_list and create_reply method

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -6,13 +6,8 @@ RSpec.describe ArticlesController, type: :controller do
   describe "GET feeds" do
     before do
       user_list = create_user_list
-
-      article1 = create(:article, user: user_list[0])
-      article2 = create(:article, user: user_list[1])
-      article3 = create(:article, user: user_list[2])
-
-      reply1 = create(:reply, user: user_list[0], article: article1)
-      reply2 = create(:reply, user: user_list[2], article: article2)
+      article_list = create_article_list(user_list)
+      create_reply(user_list, article_list)
       sign_in(user_list[0])
 
     end
@@ -20,10 +15,10 @@ RSpec.describe ArticlesController, type: :controller do
     it "check we have user count,reply count,and article count info in view template" do
       get :feeds
       expect(assigns(:user_count)).to eq(15)
-      expect(assigns(:article_count)).to eq(3)
-      expect(assigns(:reply_count)).to eq(2)
+      expect(assigns(:article_count)).to eq(15)
+      expect(assigns(:reply_count)).to eq(55)
     end
 
-    
+
   end
 end
--- spec/support/articles_helper.rb
+++ spec/support/articles_helper.rb
@@ -9,4 +9,21 @@ module ArticlesHelper
     return user_list
   end
 
+  def create_article_list(user_list)
+    article_list = []
+    user_list.each do |user|
+      article_list.push(FactoryBot.create(:article, user: user))
+    end
+    return article_list
+  end
+
+  def create_reply(user_list, article_list)
+    
+    for i in 0..9
+      ( 10-i ).times do 
+        create(:reply, user: user_list[i], article: article_list[i])
+      end
+    end
+    
+  end
 end
