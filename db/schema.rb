887cd73f54bae0aa71dfbc9b82928d26dbfabfce
spy <sp831031@gmail.com>
2018-05-07 14:40:59 +0800

    add omniauth column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502082859) do
+ActiveRecord::Schema.define(version: 20180507063823) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -88,8 +88,11 @@ ActiveRecord::Schema.define(version: 20180502082859) do
     t.string "avatar"
     t.integer "replies_count", default: 0
     t.string "authentication_token"
+    t.string "fb_uid"
+    t.string "fb_token"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fb_uid"], name: "index_users_on_fb_uid"
     t.index ["replies_count"], name: "index_users_on_replies_count"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
