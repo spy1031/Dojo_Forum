8d29273d138f707af4777cce77b91676eb3fdaf7
spy <sp831031@gmail.com>
2018-05-08 15:23:25 +0800

    update deplot linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/deploy/Dojo_Forum'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/facebook.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
